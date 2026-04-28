c1ce58567cd41177686164f425f0223d4d0da649
Andrey Shibaev <smallescontainer@gmail.com>
2026-04-29 01:48:50 +0300

    added anslbe inventory generation for created hosts

--- lxc-adventures-setup/setup.sh
+++ lxc-adventures-setup/setup.sh
@@ -4,6 +4,7 @@ INSTANCE_BASE_NAME="$(basename $(pwd))"
 INSTANCE_COUNT=3
 INSTANCE_DISTRO="ubuntu:24.04"
 SSH_KEY_PATH="/home/scont/.ssh/id_ed25519.pub"
+INVETORY_FILE_NAME="inventory.yaml"
 
 while getopts ":n:c:r" opt; do
     case $opt in
@@ -24,11 +25,19 @@ for INSTANCE in "${INSTANCES[@]}"; do
     fi
 done
 
+cat <<EOF > $INVETORY_FILE_NAME
+all:
+  children:
+    containers:
+      hosts:
+EOF
+
 for INSTANCE in "${INSTANCES[@]}"; do
     lxc launch "$INSTANCE_DISTRO" "$INSTANCE"
     echo "Waiting for ip"
     sleep 2
     INSTANCE_IP="$(lxc list $INSTANCE --columns=4 --format=csv | cut -d' ' -f1)"
+    echo >> "$INVETORY_FILE_NAME" "        $INSTANCE_IP:"
     echo "Copying ssh key to the container"
     cat "$SSH_KEY_PATH" | lxc exec "$INSTANCE" -- sh -c \
                                             "mkdir -p /root/.ssh && \
